98f495d40c7dd4a1e04fc57b3d6cc93fa7b2dbf1
Константин Абраменко <kostaabramenko695@gmail.com>
2021-03-28 16:50:36 +0700

    Fourth

--- Lab4Prog/Program.cs
+++ Lab4Prog/Program.cs
@@ -8,12 +8,43 @@ namespace Lab4Prog
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите путь: ");
-            string pathToFile = Console.ReadLine();
-            Console.Write("Введите ключевое слово: ");
-            string keyword = Console.ReadLine();
-            findFiles files = new findFiles(pathToFile, keyword) ;
-            files.GetInfo();
+        ReturnProg:
+            Console.WriteLine("Выберите: \n1) Сериализация\n2) Поиск текстового файла по ключевым словам");
+            string change = Console.ReadLine();
+            switch (change)
+            {
+                case "1":
+                    FileStream fs = new
+                    FileStream("c:\\FullNameSerialize.bin",
+                    FileMode.OpenOrCreate, FileAccess.Write);
+                    TextFile fnc = new TextFile("Ivan", "Ivanov", "Ivanovich");
+                    fnc.Print();
+                    fnc.Serialize(fs);
+                    fnc = new TextFile("Petr", "Petrov", "Petrovich");
+                    fnc.Print();
+                    fs = new
+                    FileStream("c:\\FullNameSerialize.bin",
+                    FileMode.OpenOrCreate, FileAccess.Read);
+                    fnc.Deserialize(fs);
+                    fnc.Print();
+                    break;
+
+                case "2":
+                    Console.Write("Введите путь: ");
+                    string pathToFile = Console.ReadLine();
+                    Console.Write("Введите ключевое слово: ");
+                    string keyword = Console.ReadLine();
+                    findFiles files = new findFiles(pathToFile, keyword);
+                    files.GetInfo();
+                    break;
+            }
+            Console.WriteLine("Повторить?(Да или нет)");
+            string returnProg = Console.ReadLine();
+            if (returnProg == "Да" || returnProg == "да")
+            {
+                goto ReturnProg;
+            }
+            else Console.ReadKey();
         }
     }
 }
--- Lab4Prog/textFile.cs
+++ Lab4Prog/textFile.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.IO;
-
+using System.Xml.Serialization;
 
 namespace Lab4Prog
 {
-    class textFile
+    class TextFile
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string MiddleName { get; set; }
+        public TextFile(string name, string surname, string middle)
+        {
+            Name = name; Surname = surname; MiddleName = middle;
+        }
+        public void Serialize(FileStream fs)
+        {
+            XmlSerializer bf = new XmlSerializer(this.GetType());
+            bf.Serialize(fs, this);
+            fs.Flush();
+            fs.Close();
+        }
+        public void Deserialize(FileStream fs)
+        {
+            XmlSerializer bf = new XmlSerializer(this.GetType());
+            TextFile deserialized = (TextFile)bf.Deserialize(fs);
+            Name = deserialized.Name;
+            Surname = deserialized.Surname;
+            MiddleName = deserialized.MiddleName;
+            fs.Close();
+        }
+        public void Print()
+        {
+            Console.WriteLine("Name={0} Surname={1} Middle={2}", Name, Surname, MiddleName);
+        }
     }
 }
